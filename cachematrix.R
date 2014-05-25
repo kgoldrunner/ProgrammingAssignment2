3314ac7377486dc4c8358136099ba9b8fd5d9441
kgoldrunner <kgoldrunner@users.noreply.github.com>
2014-05-25 18:19:01 -0500

    Updated cachematrix.R
    Added comments and code for the functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Caching the Mean of a Vector
+##The following can be used to assign a value to an object in an environment 
+##that is different from the current environment. Below are two functions 
+##that are used to create a special object that stores a numeric vector and cache's its mean.
 
-## Write a short comment describing this function
+
+##makeVector creates a special "vector", which is really a list containing a function to
+##set the value of the vector
+##get the value of the vector
+##set the value of the mean
+##get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the mean of the special "vector" created with the above function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m        
 }
